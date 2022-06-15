40ce616b05099ca613e09c97358d793423d4fa43
Abdussamad Khan <Abdussamad Khan@khans-desktop29>
2022-06-15 18:34:21 +0530

    first commit

---
+++ okHI/src/okHI/XDD.java
@@ -0,0 +1,14 @@
+package okHI;
+
+public class XDD {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "king is here";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+
+	}
+
+}
